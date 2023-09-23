f8bd06d4267dcbc9ad3c642448b89d860cf0aece
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-09-23 12:03:54 -0300

    Initial commit

---
+++ src/components/Modal.tsx
@@ -0,0 +1,26 @@
+// CSS
+import React from "react";
+import styles from "./Modal.module.css";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Modal = ({ children }: Props) => {
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.getElementById("modal");
+    modal!.classList.add("hide");
+  };
+
+  return (
+    <div id="modal" className="hide">
+      <div className={styles.fade} onClick={closeModal}></div>
+      <div className={styles.modal}>
+        <h2>Texto modal</h2>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
